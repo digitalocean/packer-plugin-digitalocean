1df24f864f92b6b7f456ff684f6bd667b14f3b12
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 15:22:57 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -6,30 +6,16 @@ import (
 
 	"github.com/hashicorp/packer-plugin-digitalocean/builder/digitalocean"
 	digitaloceanPP "github.com/hashicorp/packer-plugin-digitalocean/post-processor/digitalocean-import"
+	"github.com/hashicorp/packer-plugin-digitalocean/version"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.1"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(digitalocean.Builder))
 	pps.RegisterPostProcessor("import", new(digitaloceanPP.PostProcessor))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
